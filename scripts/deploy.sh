bcb090a4a61541e66b0b9585b7f7f80b0b716aee
Abdul Rahman Khan <abdulrahman.h62@gmail.com>
2024-07-23 16:28:05 +0000

    deploy.sh updated with docker images.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,15 @@
 
 # Variables
 KUBE_CONFIG="path/to/your/kubeconfig"  # Replace with actual path or use Jenkins/Kubernetes plugin for config
-REGISTRY_URL="https://hub.docker.com/repository/docker/khana88/swish-final-project/general"  # Replace with your Docker registry URL
+REGISTRY_URL="https://hub.docker.com/repositories/khana88"  # Replace with your Docker registry URL
 BUILD_NUMBER="1"  # Example build number or use dynamically from Jenkins
 
 # Build and push Docker images
-docker build -t ${REGISTRY_URL}/nodejs-image:${BUILD_NUMBER} -f Dockerfile.nodejs .
-docker push ${REGISTRY_URL}/nodejs-image:${BUILD_NUMBER}
+docker build -t https://hub.docker.com/repository/docker/khana88/swish-final-project-nodejs/general/swish-final-project-nodejs:latest -f Dockerfile.nodejs .
+docker push https://hub.docker.com/repository/docker/khana88/swish-final-project-nodejs/general/swish-final-project-nodejs:latest
 
-docker build -t ${REGISTRY_URL}/python-image:${BUILD_NUMBER} -f Dockerfile.python .
-docker push ${REGISTRY_URL}/python-image:${BUILD_NUMBER}
+docker build -t https://hub.docker.com/repository/docker/khana88/swish-final-project-python/general/swish-final-project-nodejs:latest -f Dockerfile.python .
+docker push https://hub.docker.com/repository/docker/khana88/swish-final-project-python/general/swish-final-project-nodejs:latest
 
 # Apply Kubernetes configurations
 kubectl apply -f kubernetes/deployment.yaml --kubeconfig=${KUBE_CONFIG}
